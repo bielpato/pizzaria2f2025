93480f3a852aaf6769d50410309831fd893f2190
bielpato <gbottospato992@gmail.com>
2025-09-19 07:47:50 -0300

    componente

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,15 @@
+const Produto = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>Pizza de Muçarela</li>
+                <li>Pizza de Calabresa</li>
+                <li>Pizza de Portuguesa</li>
+                <li>Pizza de Baiana</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Produto
